b0be1714ffd6728a96f7c243ad450d4a8aaa1678
Chidubem Chinwuba <chidubemchinwuba01@gmail.com>
2023-10-17 09:41:19 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Define a static route to serve static files (like index.html)
+app.use(express.static(path.join(__dirname, '/')));
+
+// Start the server
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
